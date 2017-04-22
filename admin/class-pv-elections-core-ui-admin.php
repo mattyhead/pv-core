23bae61759e7a285b704e77752d37af006e736d1
matt murphy <mattyhead@gmail.com>
2017-04-21 21:58:28 -0400

    ...

--- admin/class-pv-elections-core-ui-admin.php
+++ admin/class-pv-elections-core-ui-admin.php
@@ -117,12 +117,12 @@ class Pv_Elections_Core_Ui_Admin {
          *
          */
         
-        add_management_page(__('Phillyvotes', $this->plugin_name),'Phillyvotes','manage_options','phillyvotes', array($this, 'display_candidates_admin_page'));
-        add_submenu_page('phillyvotes', __('Candidates & Offices', $this->plugin_name), 'Candidates & Offices', 'manage_options', 'candidates-and-offices', array($this, 'display_candidates_admin_page'));
-        add_submenu_page('phillyvotes', __('Department', $this->plugin_name), 'Department', 'manage_options', 'department', array($this, 'display_department_admin_page'));
-        add_submenu_page('phillyvotes', __('Elections', $this->plugin_name), 'Elections', 'manage_options', 'results', array($this, 'display_elections_admin_page'));
-        add_submenu_page('phillyvotes', __('Offices', $this->plugin_name), 'Offices', 'manage_options', 'officers', array($this, 'display_officers_admin_page'));
-        add_submenu_page('phillyvotes', __('Voters', $this->plugin_name), 'Voters', 'manage_options', 'voters', array($this, 'display_voters_admin_page'));
+        add_admin_page(__('Phillyvotes', $this->plugin_name),'Phillyvotes','manage_options','phillyvotes', array($this, 'display_candidates_admin_page'));
+        // add_submenu_page('phillyvotes', __('Candidates & Offices', $this->plugin_name), 'Candidates & Offices', 'manage_options', 'candidates-and-offices', array($this, 'display_candidates_admin_page'));
+        // add_submenu_page('phillyvotes', __('Department', $this->plugin_name), 'Department', 'manage_options', 'department', array($this, 'display_department_admin_page'));
+        // add_submenu_page('phillyvotes', __('Elections', $this->plugin_name), 'Elections', 'manage_options', 'results', array($this, 'display_elections_admin_page'));
+        // add_submenu_page('phillyvotes', __('Offices', $this->plugin_name), 'Offices', 'manage_options', 'officers', array($this, 'display_officers_admin_page'));
+        // add_submenu_page('phillyvotes', __('Voters', $this->plugin_name), 'Voters', 'manage_options', 'voters', array($this, 'display_voters_admin_page'));
     
     }
 
