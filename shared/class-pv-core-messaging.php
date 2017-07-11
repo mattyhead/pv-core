eeea9a60c8cda46787f1bcdb6334b724efd8c252
matt murphy <mattyhead@gmail.com>
2017-07-11 09:19:39 -0400

    flesh out messaging fully

--- shared/class-pv-core-messaging.php
+++ shared/class-pv-core-messaging.php
@@ -17,49 +17,93 @@ if ( ! class_exists( 'Pv_Core_Messaging' ) ) {
 	 */
 	class Pv_Core_Messaging {
 
-		protected $message = 'your message here';
+		/**
+		 * Message to schedule
+		 *
+		 * @var        string
+		 */
+		protected $message = '';
 
+		/**
+		 * Format of messages
+		 *
+		 * @var        string
+		 */
+		protected $format = '<div class="%1$s"><p>%2$s</p></div>';
+
+		/**
+		 * Construct auto-runs queue()
+		 */
 		public function __construct() {
 
 			$this->queue();
 
 		}
 
+		/**
+		 * Write out an error message
+		 */
+		public function error() {
+
+			$class = 'notice notice-error is-dismissible';
+
+			printf( $this->format, esc_attr( $class ), esc_html( $this->message ) );
+
+		}
+
+		/**
+		 * Write out an info message
+		 */
+		public function info() {
+
+			$class = 'notice notice-info is-dismissible';
+
+			printf( $this->format, esc_attr( $class ), esc_html( $this->message ) );
+
+		}
+
+		/**
+		 * Write out a success message
+		 */
 		public function success() {
 
-			$class = 'notice notice-success';
+			$class = 'notice notice-success is-dismissible';
 
-			printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $this->message ) );
-
-		}
-
-		public function failure() {
-
-			$class = 'notice notice-failure';
-
-			printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $this->message ) );
+			printf( $this->format, esc_attr( $class ), esc_html( $this->message ) );
 
 		}
 
-		public function notice() {
+		/**
+		 * Write out a warning message
+		 */
+		public function warning() {
 
-			$class = 'notice notice-info';
+			$class = 'notice notice-warning is-dismissible';
 
-			printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $this->message ) );
+			printf( $this->format, esc_attr( $class ), esc_html( $this->message ) );
 
 		}
 
+		/**
+		 * Queue a 'pvmessage' based on param 'pvstatus'
+		 */
 		public function queue() {
 
 			if ( isset( $_REQUEST['pvstatus'] ) ) {
 
 				$this->message = isset( $_REQUEST['pvmessage'] ) ? sanitize_text_field( wp_unslash( $_REQUEST['pvmessage'] ) ) : '' ;
 				switch ( $_REQUEST['pvstatus'] ) {
+					case 'error':
+						add_action( 'admin_notices', array( $this, 'error' ), 10, 0 );
+					break;
+					case 'info':
+						add_action( 'admin_notices', array( $this, 'info' ), 10, 0 );
+					break;
 					case 'success':
 						add_action( 'admin_notices', array( $this, 'success' ), 10, 0 );
 					break;
-					case 'failure':
-						add_action( 'admin_notices', array( $this, 'failure' ), 10, 0 );
+					case 'warning':
+						add_action( 'admin_notices', array( $this, 'warning' ), 10, 0 );
 					break;
 				}
 			}
