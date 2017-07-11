dad0c01f0d9e11f58cb017b3287347987cb9be78
matt murphy <mattyhead@gmail.com>
2017-07-11 10:35:35 -0400

    ...

--- shared/class-pv-core-model.php
+++ shared/class-pv-core-model.php
@@ -122,7 +122,7 @@ if ( ! class_exists( 'Pv_Core_Messaging' ) ) {
 		 * @param      mixed $data   The data.
 		 */
 		public function insert( $data ) {
-			$this->dbase->insert( $this->dbase->prefix . $this->tablename, $data );
+			return $this->dbase->insert( $this->dbase->prefix . $this->tablename, $data );
 		}
 
 		/**
