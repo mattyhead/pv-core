a3a98f00058065d634e3fcd3ac45e0ff790a6777
matt murphy <mattyhead@gmail.com>
2017-07-19 16:20:35 -0400

    wiring up core browser includes

---
+++ js/pv-core-admin.js
@@ -0,0 +1,44 @@
+(function($, w) {
+    'use strict';
+
+    $(function() {
+
+        // tabs
+        var $tabBoxes = $('.pv-metaboxes'),
+            $navTabWrapper = $('.nav-tab-wrapper'),
+            $navTab = $('.nav-tab'),
+            $tabLinkActive,
+            $currentTab,
+            $currentTabLink,
+            $tabContent,
+            $hash;
+
+        // Tabs on load
+        if((w.location.hash).length){
+            $hash = w.location.hash;
+            $tabBoxes.addClass('hidden');
+            $currentTab = $($hash).toggleClass('hidden');
+            $navTab.removeClass('nav-tab-active');
+            $('.nav-tab[href='+$hash+']').addClass('nav-tab-active');
+        }
+
+        //Tabs on click
+        $navTabWrapper.on('click', 'a', function(e){
+            var $this = $(this);
+            e.preventDefault();
+            $tabContent = $this.attr('href');
+            $navTab.removeClass('nav-tab-active');
+            $tabBoxes.addClass('hidden');
+            $currentTab = $($tabContent).toggleClass('hidden');
+            $this.addClass('nav-tab-active');
+             if(history.pushState) {
+                history.pushState(null, null, $tabContent);
+            }
+            else {
+                location.hash = $tabContent;
+            }
+        })
+
+    });
+
+})(jQuery, window);
