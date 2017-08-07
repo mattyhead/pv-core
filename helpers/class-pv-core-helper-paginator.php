3a28eb8f39b6270d091d5ba378a5c044c17b2b33
matt murphy <mattyhead@gmail.com>
2017-08-07 10:10:21 -0400

    making the footer a unified element

--- helpers/class-pv-core-helper-paginator.php
+++ helpers/class-pv-core-helper-paginator.php
@@ -43,9 +43,9 @@ if ( ! class_exists( 'Pv_Core_Helper_Paginator' ) ) {
 		}
 
 		/**
-		 * Gets the list footer.
+		 * Gets the list navigation.
 		 */
-		public function get_list_footer() {
+		public function get_footer() {
 
 			$first = '&lt;&lt;first';
 			$previous = '&lt;previous';
@@ -71,14 +71,28 @@ if ( ! class_exists( 'Pv_Core_Helper_Paginator' ) ) {
 			}
 
 			?>
-				<span><?php echo $first ; ?></span>
-				<span>|</span>
-				<span><?php echo $previous ; ?></span>
-				<span>|</span>
-				<span><?php echo $next ; ?></span>
-				<span>|</span>
-				<span><?php echo $last ; ?></span>
+				<span class="alignleft row-actions visible">
+					<span><?php echo $first ; ?></span>
+					<span>|</span>
+					<span><?php echo $previous ; ?></span>
+					<span>|</span>
+					<span><?php echo $next ; ?></span>
+					<span>|</span>
+					<span><?php echo $last ; ?></span>
+				</span>
+				<span class="aligncenter">
+					Page <?php echo esc_html( $this->current ); ?> of <?php echo esc_html( $this->last ); ?>.
+				</span>
+				<span class="alignright row-actions visible">
+					<span><a target="_blank" href="<?php echo esc_attr( WP_PLUGIN_URL . '/' . $this->plugin_name . '/admin/export.php?' . '&current=' . $this->pagination->current . '_wpnonce=' . wp_create_nonce( $this->plugin_name . '_admin_export' ) ); ?>" >export all</a></span>
+					<span>|</span>
+					<span class="trash"><a href="<?php echo esc_attr( admin_url( 'admin-post.php?action=' . $this->plugin_name . '_admin_delete_all' . '&current=' . $this->pagination->current . '&_wpnonce=' . wp_create_nonce( $this->plugin_name . '_admin_delete_all' ) ) ); ?>" >delete all</a></span>
+				</span>
 			<?php
 		}
+
 	}
+
 }
+
+
